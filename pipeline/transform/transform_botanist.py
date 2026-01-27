9c6a9ee0d97453ea6aba213127f9cc7e9bf41e14
Kieran <k1phelan234@gmail.com>
2026-01-27 13:48:24 +0000

    Transformed botanist data with testing

---
+++ pipeline/transform/transform_botanist.py
@@ -0,0 +1,59 @@
+"""Script to transform and clean plant data."""
+import re
+import pandas as pd
+
+
+def load_data(file_path: str):
+    """Load data from a CSV file into a DataFrame."""
+    df = pd.read_csv(file_path)
+    return df
+
+
+def get_botanists(df: pd.DataFrame) -> pd.DataFrame:
+    """Extract unique botanist details from the plant data."""
+    botanists_df = df[['botanist_name',
+                       'botanist_email',
+                       'botanist_phone']].dropna().drop_duplicates()
+
+    botanists_df['botanist_phone'] = botanists_df['botanist_phone'].apply(
+        clean_phone_number)
+
+    botanists_df = botanists_df.reset_index(drop=True)
+    botanists_df.insert(0, 'botanist_id', range(1, len(botanists_df) + 1))
+    return botanists_df
+
+
+def clean_phone_number(phone: str) -> str:
+    """Clean and standardise phone numbers."""
+    if pd.isna(phone) or phone == '':
+        return None
+
+    parts = phone.split('x')
+    number = parts[0]
+    country_code = 0
+
+    number = re.sub(r'[+.\(\)\-]', '', number)
+
+    if len(number[:-10]) > 0:
+        country_code = int(number[:-10])
+
+    number = number[-10:-7] + '-' + number[-7:-4] + '-' + number[-4:]
+
+    if country_code > 0:
+        number = '+' + str(country_code) + '-' + number
+
+    if len(parts) == 1:
+        return number
+
+    return number + 'x' + parts[1]
+
+
+if __name__ == "__main__":
+    df = load_data("test_data.csv")
+
+    data = {}
+
+    data["botanists"] = get_botanists(df)
+
+    print(data)
+    get_botanists(df).to_csv("test_clean_data.csv", index=False)
