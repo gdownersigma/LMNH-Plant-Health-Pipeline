3871c56c69fb7efe1a7ec93a082c5637dd7a36d9
Sheikh Abdul Halim <trainee.sheikh.abdul.halim@sigmalabs.co.uk>
2026-01-27 11:08:39 +0000

    Add trust and template permission policy for schedulers.

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,14 +24,67 @@ data "aws_subnet" "c21-public-subnet-c" {
   id = var.SUBNET_ID_C
 }
 
+# ECS Cluster
+data "aws_ecs_cluster" "target-cluster" {
+    cluster_name = var.CLUSTER_NAME
+}
 
 # S3 Bucket
 
 # Create new bucket
 resource "aws_s3_bucket" "plant-storage" {
     bucket = "${var.BASE_NAME}-plant-storage"
-    
     tags = {
         Name = "${var.BASE_NAME} Plant Storage Bucket"
+    }
+}
+
+# ETL Eventbridge Schedule
+
+## Trust Policy
+data  "aws_iam_policy_document" "schedule-trust-policy" {
+    statement {
+        effect = "Allow"
+        principals {
+            type        = "Service"
+            identifiers = ["scheduler.amazonaws.com"]
+        }
+        actions = ["sts:AssumeRole"]
+    }
+}
+
+## Permission Policy
+data  "aws_iam_policy_document" "schedule-permissions-policy" {
+
+    statement {
+        effect = "Allow"
+        #resources = ["${task definition arn for etl pipeline lambda}:*"]
+        actions = ["ecs:RunTask"]
+        condition {
+            test     = "ArnLike"
+            variable = "ecs:cluster"
+            values   = [data.aws_ecs_cluster.target-cluster.arn]
+        }
+    }
+
+    statement {
+        effect = "Allow"
+        resources = ["*"]
+        actions = ["iam:PassRole"]
+        condition {
+            test     = "StringLike"
+            variable = "iam:PassedToService"
+            values   = ["ecs-tasks.amazonaws.com"]
+        }
+    }
+
+    statement {
+        effect = "Allow"
+        resources = ["arn:aws:logs:*:*:*"]
+        actions = [
+            "logs:CreateLogStream",
+            "logs:PutLogEvents",
+            "logs:CreateLogGroup"
+        ]
     }
 }
