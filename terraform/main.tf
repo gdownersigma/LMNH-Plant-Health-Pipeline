c094689c01bc67aa8fc57c94aaecc9215200eef9
Sheikh Abdul Halim <trainee.sheikh.abdul.halim@sigmalabs.co.uk>
2026-01-29 11:06:28 +0000

    Add task definition.

--- terraform/main.tf
+++ terraform/main.tf
@@ -371,4 +371,37 @@ resource "aws_cloudwatch_log_group" "ecs_dashboard_logs" {
 
 # ECS Task Definition
 
+resource "aws_ecs_task_definition" "streamlit" {
+  family                   = "${var.BASE_NAME}-streamlit-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = 512
+  memory                   = 1024
+  execution_role_arn       = aws_iam_role.ecs_task_execution.arn
 
+  container_definitions = jsonencode([
+    {
+      name  = "${var.BASE_NAME}-streamlit"
+      image = data.aws_ecr_image.dashboard-image.image_uri
+
+      portMappings = [
+        {
+          containerPort = 8501
+          hostPort      = 8501
+          protocol      = "tcp"
+        }
+      ]
+
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          "awslogs-group"         = aws_cloudwatch_log_group.ecs_dashboard_logs.name
+          "awslogs-region"        = var.DEFAULT_REGION
+          "awslogs-stream-prefix" = "ecs"
+        }
+      }
+
+      essential = true
+    }
+  ])
+}
